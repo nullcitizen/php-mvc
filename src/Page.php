1e68f4897ff84bf558a4a610a32faf6206395abf
Joe Cochran <j_cochran@me.com>
2015-04-08 13:22:26 -0700

    phpdocing all the things

--- src/Page.php
+++ src/Page.php
@@ -1,19 +1,47 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Basic Page model
+ *
+ * PHP version 5.6
+ *
+ * @category Common
+ * @package  ExampleMVC
+ * @author   Joe Cochran <j_cochran@me.com>
+ * @license  http://opensource.org/licenses/BSD-2-Clause BSD 2 Clause 
+ * @link     http://joecochran.github.io
+ */
 namespace Cochran;
 
-use \PDO; //is this preferable to $this->db = new \PDO?
+use \PDO;
 
+/**
+ * Basic Page model
+ *
+ * @category Common
+ * @package  ExampleMVC
+ * @author   Joe Cochran <j_cochran@me.com>
+ * @license  http://opensource.org/licenses/BSD-2-Clause BSD 2 Clause 
+ * @link     http://joecochran.github.io
+ */
 class Page
 {
 
     protected $id;
 
     protected $slug;
-
+    
+    /**
+     * Create a new Page Instance
+     */ 
     public function __construct()
     {
         try {
-            $this->db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+            $this->db = new PDO(
+                "mysql:host=".DB_HOST.";dbname=".DB_NAME,
+                DB_USER, DB_PASS
+            );
             $this->db->setAttribute(PDO::ATTR_ERRMODE, 'PDO::ERRMODE_EXCEPTION');
         } catch(\Exception $e) {
             echo "DB Error";
@@ -21,6 +49,13 @@ class Page
         }
     }
 
+    /**
+     * Get page by slug
+     *
+     * @param string $slug to look up in database
+     *
+     * @return array Returns the page as an associative array
+     */
     public function get($slug)
     {
         try {
@@ -37,13 +72,28 @@ class Page
         return $page;
     }
 
+    /**
+     * Create new page
+     *
+     * @param array $data information to create page with 
+     *
+     * @return null
+     */
     public function create(array $data)
     {
-        $sql = $this->db->prepare("INSERT INTO PAGES (title, slug, description, content) VALUES (:title, :slug, :description, :content)");
-        $sql->bindParam(':title', $data['title']);
-        $sql->bindParam(':slug', $data['slug']);
-        $sql->bindParam(':description', $data['description']);
-        $sql->bindParam(':content', $data['content']);
-        $sql->execute();
+        try {
+            $sql = $this->db->prepare(
+                "INSERT INTO PAGES (title, slug, description, content) 
+                 VALUES (:title, :slug, :description, :content)"
+            );
+            $sql->bindParam(':title', $data['title']);
+            $sql->bindParam(':slug', $data['slug']);
+            $sql->bindParam(':description', $data['description']);
+            $sql->bindParam(':content', $data['content']);
+            $sql->execute();
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            die();
+        }
     }
 }
--- src/Page/PageController.php
+++ src/Page/PageController.php
@@ -1,10 +1,35 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Basic Page Controller
+ *
+ * PHP version 5.6
+ *
+ * @category Common
+ * @package  ExampleMVC
+ * @author   Joe Cochran <j_cochran@me.com>
+ * @license  http://opensource.org/licenses/BSD-2-Clause BSD 2 Clause 
+ * @link     http://joecochran.github.io
+ */
 namespace Cochran\Page;
 
 use Cochran\Page;
 
+/**
+ * Basic Page Controller
+ *
+ * @category Common
+ * @package  ExampleMVC
+ * @author   Joe Cochran <j_cochran@me.com>
+ * @license  http://opensource.org/licenses/BSD-2-Clause BSD 2 Clause 
+ * @link     http://joecochran.github.io
+ */
 class PageController
 {
+    /**
+     * Constructor
+     */ 
     public function __construct()
     {
         $this->page = new Page();
@@ -12,30 +37,51 @@ class PageController
         $this->templates = new \League\Plates\Engine('../resources/views');
     }
 
+    /**
+     * Return a list of all pages
+     *
+     * @return null
+     */
     public function index()
     {
         echo 'list all pages';
     }
 
+    /**
+     * Load form to create new page
+     *
+     * @return null
+     */
     public function create()
     {
         echo $this->templates->render('page/create');
     }
 
+    /**
+     * Store Page 
+     *
+     * @return null
+     */
     public function store()
     {
         $this->page->create($_POST);
         header('Location: //page/create');
     }
 
+    /**
+     * Get page by slug
+     *
+     * @param string $slug to look up in database
+     *
+     * @return null
+     */
     public function show($slug = 'home')
     {
         $page = $this->page->get($slug);
-        if($page) {
+        if ($page) {
             $page['content'] = $this->converter->convertToHtml($page['content']);
             echo $this->templates->render($slug, ['data' => $page]);
-        } else {
-            //http_response_code(404);
+        } else { 
             echo $this->templates->render('404');
         }    
     }
